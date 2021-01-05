9c4f345aae10d254c9e2f2a8db5083db17a9eb76
MoeBazziGIT <bazzimoe3@gmail.com>
2021-01-05 14:03:35 -0500

    added value dependancy in side effect hook in usePrevious custom hooks
    Side effect only needs to get triggered when value is updated, not every render

--- customHooks.js
+++ customHooks.js
@@ -1,9 +1,13 @@
-import { useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
+// custom hook for getting previous value
 export function usePrevious(value) {
+
   const ref = useRef();
   useEffect(() => {
     ref.current = value;
-  });
+  }, [value]);
+
   return ref.current;
+
 }
