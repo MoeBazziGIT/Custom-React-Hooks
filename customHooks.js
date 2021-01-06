570ee5bbda591e28a1c4e7e991faf5c4827c1dd0
MoeBazziGIT <bazzimoe3@gmail.com>
2021-01-06 01:28:52 -0500

    added useAutoUmountingComponent custom hook

--- customHooks.js
+++ customHooks.js
@@ -26,3 +26,33 @@ export function useDependentState(dependancyValue, defaultState){
   return [value, setValue];
 
 }
+
+
+
+// unmounts and remounts component on update of dependancies
+//  leave out dependies parameter in order unmount/mount on every render of parent component of component
+
+// make sure to perform proper clean ups in component before unmounting:
+//    -> return a callback method in useEffect(() => return cleanUpMethod, []) ie. componentWillUnmount
+export function useAutoUmountingComponent(newComponent, dependancies){
+
+  var [component, setComponent] = useState(-1);
+
+  useEffect(() => {
+
+    setComponent(null)
+
+  }, dependancies);
+
+
+  useEffect(() => {
+
+    if(component === null){
+      setComponent(newComponent);
+    }
+
+  }, [component])
+
+  return component;
+
+}
