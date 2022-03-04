7d716dad2f720ee123a8be9d24be86584f9c0b3b
MoeBazziGIT <bazzimoe3@gmail.com>
2022-03-04 09:59:59 -0500

    added useTraceUpdate

--- customHooks.js
+++ customHooks.js
@@ -1148,4 +1148,19 @@ function useFileDialog(onSelectFile, options){
 	return { openFileDialog };
 };
 
-const { openFileDialog } = useFileDialog()
+// https://stackoverflow.com/a/51082563/10163060
+function useTraceUpdate(props){
+  const prev = useRef(props);
+  useEffect(() => {
+    const changedProps = Object.entries(props).reduce((ps, [k, v]) => {
+      if (prev.current[k] !== v) {
+        ps[k] = [prev.current[k], v];
+      }
+      return ps;
+    }, {});
+    if (Object.keys(changedProps).length > 0) {
+      console.log('Changed props:', changedProps);
+    }
+    prev.current = props;
+  });
+}
